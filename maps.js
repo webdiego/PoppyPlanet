73643962c2ce55e54bcc4cdf4a5a30879a376e7c
Your Name <webdiegomassarini@gmail.com>
2020-12-07 16:18:57 +1100

    98% completed

---
+++ maps.js
@@ -0,0 +1,28 @@
+var script = document.createElement('script');
+script.src = "https://maps.googleapis.com/maps/api/js?key=AIzaSyAYr-rPqdhSl4nvATxVyuelSZ8yYfrj1Ps&callback=initMap&libraries=&v=weekly";
+script.defer = true;
+
+let map;
+
+function initMap() {
+  const myLatLng = { lat:-33.881630, lng:151.218590}
+ const  map = new google.maps.Map(document.getElementById("map"), {
+    center: myLatLng,
+    zoom: 17,
+     
+
+  });
+
+  new google.maps.Marker({
+    position: myLatLng,
+    map,
+    title: "Poppy Planet",
+    zoomControl: false,
+
+    
+  });
+  
+  
+}
+
+document.head.appendChild(script);
