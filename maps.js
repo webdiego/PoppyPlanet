9d12251ae78cd113795dea4ffeb51f7f7d3b09cf
Your Name <webdiegomassarini@gmail.com>
2020-12-12 08:42:45 +1100

    menu resized

--- maps.js
+++ maps.js
@@ -1,16 +1,15 @@
-var script = document.createElement('script');
-script.src = "https://maps.googleapis.com/maps/api/js?key=AIzaSyAYr-rPqdhSl4nvATxVyuelSZ8yYfrj1Ps&callback=initMap&libraries=&v=weekly";
+var script = document.createElement("script");
+script.src =
+  "https://maps.googleapis.com/maps/api/js?key=AIzaSyAYr-rPqdhSl4nvATxVyuelSZ8yYfrj1Ps&callback=initMap&libraries=&v=weekly";
 script.defer = true;
 
 let map;
 
 function initMap() {
-  const myLatLng = { lat:-33.881630, lng:151.218590}
- const  map = new google.maps.Map(document.getElementById("map"), {
+  const myLatLng = { lat: -33.88163, lng: 151.21859 };
+  const map = new google.maps.Map(document.getElementById("map"), {
     center: myLatLng,
     zoom: 17,
-     
-
   });
 
   new google.maps.Marker({
@@ -18,11 +17,7 @@ function initMap() {
     map,
     title: "Poppy Planet",
     zoomControl: false,
-
-    
   });
-  
-  
 }
 
 document.head.appendChild(script);
