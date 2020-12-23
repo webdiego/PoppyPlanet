30c5444fe7a268c259838284e784f9e0e17baede
Your Name <webdiegomassarini@gmail.com>
2020-12-23 13:33:05 +1100

    lazy loading img

--- maps.js
+++ maps.js
@@ -21,3 +21,30 @@ function initMap() {
 }
 
 document.head.appendChild(script);
+
+
+const imgTargets = document.querySelectorAll('img[data-src]');
+
+const loadImg = function (entries, observer) {
+  const [entry] = entries;
+  console.log(entry);
+  if (!entry.isIntersecting) return;
+  //replace src with data-src
+
+  entry.target.src = entry.target.dataset.src;
+
+  entry.target.addEventListener('load', function(e){
+    entry.target.classList.remove('lazy-img')
+  })
+
+  observer.unobserve(entry.target)
+};
+
+const imgObserver = new IntersectionObserver(loadImg, {
+  root: null,
+  threshold: 0.4,
+});
+
+imgTargets.forEach(function (img) {
+  return imgObserver.observe(img);
+});
